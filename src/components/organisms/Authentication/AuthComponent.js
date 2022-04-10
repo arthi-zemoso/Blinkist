ab0e23f5eb0ead626dcfc13c0ce8c5f4d473651f
ARthi ANeel <arthianeel2000@gmail.com>
2022-04-10 11:19:05 +0530

    added

--- src/components/organisms/Authentication/AuthComponent.js
+++ src/components/organisms/Authentication/AuthComponent.js
@@ -2,9 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from '../LogoutButton'
 import {Typography} from '@mui/material'
 const AuthComponent=()=>{
-      
       const {user}=useAuth0();
-
   return (
       <div className="container" sx={{
         width:"40px",
